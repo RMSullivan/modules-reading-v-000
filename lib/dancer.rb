5cc61976b154dfe94a794b80b0bebcdc1c24c583
Rita Sullivan <ritarojas@mac.com>
2018-07-08 21:25:10 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,11 +1,16 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative 'fancy_dance.rb'
 
 class Dancer
-  include Dancer
-  extend MetaDancing
+
+include Dance
+extend MetaDancing
+ 
   attr_accessor :name
+ 
   def initialize(name)
-    @name=name
+    @name = name
   end
+
 end
